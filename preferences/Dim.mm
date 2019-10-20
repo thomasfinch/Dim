f0fff41fdeb52af776f9ee22045ea30d9ffb697c
Michael Lema <mel526@nyu.edu>
2019-10-20 17:57:50 -0400

    Fixed deprecated issues

--- preferences/Dim.mm
+++ preferences/Dim.mm
@@ -7,31 +7,31 @@
 @implementation DimListController
 
 -(void)TFTwitterButtonTapped {
-	UIApplication *app = [UIApplication sharedApplication];
-	NSURL *tweetbot = [NSURL URLWithString:@"tweetbot:///user_profile/tomf64"];
-	if ([app canOpenURL:tweetbot])
-		[app openURL:tweetbot];
-	else {
-		NSURL *twitterapp = [NSURL URLWithString:@"twitter:///user?screen_name=tomf64"];
-		if ([app canOpenURL:twitterapp])
-			[app openURL:twitterapp];
-		else {
-			NSURL *twitterweb = [NSURL URLWithString:@"http://twitter.com/tomf64"];
-			[app openURL:twitterweb];
-		}
-	}
+    UIApplication *app = [UIApplication sharedApplication];
+    NSURL *tweetbot = [NSURL URLWithString:@"tweetbot:///user_profile/tomf64"];
+    if ([app canOpenURL:tweetbot])
+        [app openURL:tweetbot options:@{} completionHandler:nil];
+    else {
+        NSURL *twitterapp = [NSURL URLWithString:@"twitter:///user?screen_name=tomf64"];
+        if ([app canOpenURL:twitterapp])
+            [app openURL:twitterapp  options:@{} completionHandler:nil];
+        else {
+            NSURL *twitterweb = [NSURL URLWithString:@"http://twitter.com/tomf64"];
+            [app openURL:twitterweb  options:@{} completionHandler:nil];
+        }
+    }
 }
 
 -(void)GithubButtonTapped {
-	NSURL *githubURL = [NSURL URLWithString:@"https://github.com/thomasfinch/Dim"];
-	[[UIApplication sharedApplication] openURL:githubURL];
+    NSURL *githubURL = [NSURL URLWithString:@"https://github.com/thomasfinch/Dim"];
+    [[UIApplication sharedApplication] openURL:githubURL  options:@{} completionHandler:nil];
 }
 
 - (id)specifiers {
-	if(_specifiers == nil) {
-		_specifiers = [[self loadSpecifiersFromPlistName:@"Dim" target:self] retain];
-	}
-	return _specifiers;
+    if(_specifiers == nil) {
+        _specifiers = [[self loadSpecifiersFromPlistName:@"Dim" target:self] retain];
+    }
+    return _specifiers;
 }
 
 @end
